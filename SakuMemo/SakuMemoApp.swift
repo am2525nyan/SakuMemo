6ca75e23b45b010f235d3afcda1b503939112d06
saki0411 <chipdale411@gmail.com>
2025-04-18 17:48:34 +0900

    Initial Commit

---
+++ SakuMemo/SakuMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SakuMemoApp.swift
+//  SakuMemo
+//
+//  Created by saki on 2025/04/18.
+//
+
+import SwiftUI
+
+@main
+struct SakuMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
