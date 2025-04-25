c438f076842cc6b2d79d434aa37202e5aa360699
saki0411 <chipdale411@gmail.com>
2025-04-25 15:29:06 +0900

    add-content-palameter

--- SakuMemo/AppIntent/MemoEntity.swift
+++ SakuMemo/AppIntent/MemoEntity.swift
@@ -9,29 +9,35 @@ import Foundation
 import AppIntents
 import SwiftUI
 
-struct OpenAppIntent: AppIntent{
-    static let title: LocalizedStringResource = "アプリを開く"
-    static var openAppWhenRun: Bool = true
-    
-    @MainActor
-    
-    func perform() async throws -> some IntentResult {
-        
-        return .result()
-        
-    }
-}
+
 
 struct Shortcut: AppShortcutsProvider {
     static var appShortcuts: [AppShortcut] {
         AppShortcut(
-            intent: OpenAppIntent(),
+            intent: AddMemoIntent(),
             phrases: [
                 "\(.applicationName)の画面を開いて",
                 "\(.applicationName)の画面を見せて",
                 "\(.applicationName)をお願い"
             ],
-            shortTitle: "アプリを開く",
-            systemImageName: "face.smiling.inverse")
+            shortTitle: "メモを追加する",
+            systemImageName: "pencil")
+    }
+}
+
+
+struct AddMemoIntent: AppIntent {
+    static let title: LocalizedStringResource = "メモを追加"
+    
+    static var openAppWhenRun: Bool = true
+    @Parameter(title: "メモの内容")
+    var content: String
+    
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        print("入力されたメモ：\(content)")
+        
+              // ここで保存処理などを書く
+              return .result()
     }
 }
