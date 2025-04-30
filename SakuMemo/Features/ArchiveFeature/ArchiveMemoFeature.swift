d930253342c3bfebc6212222dbf3afdfe30684c0
saki0411 <chipdale411@gmail.com>
2025-04-30 11:27:58 +0900

    add-tabView

---
+++ SakuMemo/Features/ArchiveFeature/ArchiveMemoFeature.swift
@@ -0,0 +1,91 @@
+//
+//  TaskReducer.swift
+//  SakuMemo
+//
+//  Created by saki on 2025/04/18.
+//
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct ArchiveMemoFeature {
+    
+    @ObservableState
+    struct State{
+        var memo = Memo(text: "")
+        var text: String = ""
+    }
+    enum Action: BindableAction {
+        case binding(BindingAction<State>)
+        case addMemo
+        case gemini
+        case geminiSuccess(MemoAnalysisResult)
+        case geminiError
+        case deleteMemo(Memo)
+        case deleteAllMemos
+        case archive(Memo)
+    }
+    @Dependency(\.swiftDataRepository) var swiftDataRepository
+    @Dependency(\.geminiRepository) var geminiRepository
+    
+    var body: some ReducerOf <Self> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .addMemo:
+                let memo = Memo(text: state.text)
+                state.memo = memo
+                state.text = ""
+                return .run { send in
+                    
+                    do{
+                        await send(.gemini)
+                    }
+                    
+                    
+                }
+            case .gemini:
+                let text = state.memo.text
+                return .run { send in
+                    if let result = await geminiRepository.gemini(for: text) {
+                        await send(.geminiSuccess(result))
+                    } else {
+                        print("⚠️ Geminiの解析に失敗")
+                        await send(.geminiError)
+                    }
+                }
+                
+            case .geminiSuccess(let result):
+                state.memo.priorityValue = result.importance
+                state.memo.category = result.category
+                let memo = state.memo
+                
+                return .run { send in
+                    try await swiftDataRepository.addMemo(newMemo: memo)
+                }
+                
+            case .deleteAllMemos:
+                return .run { send in
+                    try await swiftDataRepository.deleteAllMemos()
+                }
+                
+            case .binding(_):
+                return .none
+            case .geminiError:
+                let memo = state.memo
+                state.text = ""
+                return .run { send in
+                    try await swiftDataRepository.addMemo(newMemo: memo)
+                }
+            case .deleteMemo(let memo):
+                return .run { send in
+                    try await swiftDataRepository.deleteMemo(memo: memo)
+                }
+            case .archive(let memo):
+                memo.isArchived = true
+                return .none
+            }
+        }
+    }
+}
---
+++ SakuMemo/Features/ArchiveFeature/ArchiveMemoView.swift
@@ -0,0 +1,66 @@
+//
+//  TaskView.swift
+//  SakuMemo
+//
+//  Created by saki on 2025/04/18.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import SwiftData
+
+struct ArchiveMemoView: View {
+    @Bindable var store: StoreOf<ArchiveMemoFeature>
+    @Environment(\.scenePhase) var scenePhase
+    @Query(filter: #Predicate<Memo>{$0.isArchived == false},sort: \Memo.createdAt, order: .reverse) var memos: [Memo]
+    var body: some View {
+        VStack {
+            HStack{
+                TextField("メモを入力", text: $store.text)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button(action: {
+                    store.send(.addMemo)
+                }, label:
+                        {
+                    Image(systemName: "paperplane.fill")
+                })
+                
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 20)
+            
+            List {
+                ForEach(memos) { memo in
+                    MemoCellView(memo: memo)
+                        .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                            Button(role: .destructive) {
+                                store.send(.deleteMemo(memo))
+                            } label: {
+                                Text("削除")
+                            }
+                        }
+                        .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                            Button(role: .destructive) {
+                                store.send(.archive(memo))
+                                
+                            } label: {
+                                Text("アーカイブ")
+                            }
+                            .tint(.cyan)
+                        }
+                }
+                
+            }
+            .listStyle(PlainListStyle())
+        }
+    }
+}
+
+#Preview {
+    MemoView(store:
+            .init(initialState: MemoFeature.State(),
+                  reducer: {
+        MemoFeature()
+    }))
+}
