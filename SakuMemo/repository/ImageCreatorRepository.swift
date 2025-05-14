53dcfbbb21cdea2fd647471b745d02710c4fcbd9
saki0411 <chipdale411@gmail.com>
2025-05-14 11:25:28 +0900

    imagecreatorでプロンプトに沿った画像を返せるようにした

--- SakuMemo/repository/ImageCreatorRepository.swift
+++ SakuMemo/repository/ImageCreatorRepository.swift
@@ -7,35 +7,51 @@
 
 import Foundation
 import ImagePlayground
+import CoreGraphics
+import Dependencies
+
 
 final class ImageCreatorRepository {
     
     static var shared = ImageCreatorRepository()
     
-    func generateImage()async throws {
+    func generateImage(text: String)async throws -> CGImage? {
         do {
             let creator = try await ImageCreator()
-            guard let style = creator.availableStyles.first else { return }
-
-
+            guard let style = creator.availableStyles.first else { return nil }
+            
+            
             let images = creator.images(
-                for: [.text("A cat wearing mittens.")],
+                for: [.text(text)],
                 style: style,
-                limit: 4)
-
-
-            // Receive the images.
+                limit: 1)
+            
             for try await image in images {
                 let anImage = image.cgImage
-
-
-                // Do something with the image.
+                return anImage
             }
+            
+            
+            
+            // Receive the images.
+            
         }
         catch ImageCreator.Error.notSupported {
-            print("Image creation not supported on the current device.")
+            print("このデバイスでは画像生成はできません")
+        } catch ImageCreator.Error.creationFailed {
+            print("画像生成に失敗しました")
+        } catch {
+            print("エラー: \(error.localizedDescription)")
         }
-
-        
+        return nil
+    }
+}
+struct ImageCreatorRepositoryKey: DependencyKey{
+    static var liveValue = ImageCreatorRepository()
+}
+extension DependencyValues{
+    var imageCreatorRepository:ImageCreatorRepository{
+        get{self[ImageCreatorRepositoryKey.self]}
+        set{self[ImageCreatorRepositoryKey.self] = newValue}
     }
 }
