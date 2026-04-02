b2147abc0349c4704468ae385710199bc8e7d336
am2525nyan <chipdale411@gmail.com>
2026-04-02 18:42:57 +0900

    🚀previewが表示できるように
    # ==== Format ====
    # :emoji: Subject
    
    # Commit body...
    
    # ==== Emojis ====
    # ✨ 新規機能追加 :sparkles:
    # 🐛 バグ修正 :bug:
    # 👍 機能修正 :+1:
    # ✏️ タイポなどの修正 :pencil2:
    # 🎨 リファクタリング :art:
    # 🔥 ファイル削除 :fire:
    # 📝 ドキュメント追加 :memo:
    # 🔉 ログ追加 :sound:
    # ✅ テストの追加 :white_check_mark:
    # 🚀 パフォーマンス改善 :rocket:
    # 🚧 コメントアウトなどの無効化 :construction:
    # 🔇 ログ削除 :mute:
    # 👮 セキュリティ関連の改善 :cop:
    # 🔖 バージョンアップ :bookmark:
    # 🎉 イニシャルコミット :tada:
    
    # ==== Messages ====
    # 新規(ファイル)機能追加 Add
    # 機能修正、既存機能追加（バグではない) Update
    # リファクタリング Clean
    # 削除など Remove
    # バグ修正 Fix
    # バージョンアップ Upgrade

--- SakuMemoPackage/Package.swift
+++ SakuMemoPackage/Package.swift
@@ -8,16 +8,25 @@ let package = Package(
     platforms: [.iOS(.v18), .macOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SakuMemoPackage",
-            targets: ["AppFeature"]
-        )
+        .library(name: "AppFeature", targets: ["AppFeature"]),
+                .library(name: "AddMemoFeature", targets: ["AddMemoFeature"]),
+                .library(name: "MemoFeature", targets: ["MemoFeature"]),
+                .library(name: "ArchiveFeature", targets: ["ArchiveFeature"]),
+                .library(name: "MemoDetailFeature", targets: ["MemoDetailFeature"]),
+                .library(name: "SettingsFeature", targets: ["SettingsFeature"]),
+                .library(name: "SubscriptionFeature", targets: ["SubscriptionFeature"]),
+                .library(name: "SharedModel", targets: ["SharedModel"]),
+                .library(name: "Repository", targets: ["Repository"]),
+                .library(name: "RepositoryProtocol", targets: ["RepositoryProtocol"]),
+                .library(name: "Utils", targets: ["Utils"]),
+                .library(name: "Components", targets: ["Components"]),
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "1.25.4"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.10.2"),
         .package(url: "https://github.com/exyte/PopupView", exact: "4.1.7"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk", exact: "12.0.0"),
+//        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "11.13.0"),
     ],
     targets: [
         .target(
@@ -26,7 +35,7 @@ let package = Package(
         ),
         .target(
             name: "AppFeature",
-            dependencies: [.composableArchitecture, "MemoFeature", "ArchiveFeature", "SettingsFeature"]
+            dependencies: [.composableArchitecture, "MemoFeature", "ArchiveFeature", "SettingsFeature", "Components", "Utils"]
         ),
         .target(
             name: "AppIntent",
@@ -79,6 +88,7 @@ extension Target.Dependency {
     static var popupView: Self { .product(name: "PopupView", package: "PopupView") }
     static var firebaseAI: Self { .product(name: "FirebaseAI", package: "firebase-ios-sdk") }
     static var FirebaseAppCheck: Self { .product(name: "FirebaseAppCheck", package: "firebase-ios-sdk") }
+    static var googleMobileAds: Self { .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads") }
 }
 
 for target in package.targets {
